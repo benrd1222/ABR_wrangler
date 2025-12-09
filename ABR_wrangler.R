7cd9df6be64f08e834838a0fc35d4d77b2ee107b
Benjamin <benjad@med.umich.edu>
2025-12-09 15:41:05 -0500

    Nearly done with rework, on to the new feature.

--- ABR_wrangler.R
+++ ABR_wrangler.R
@@ -12,6 +12,7 @@ message("Finding samples...")
 
 samples <- list.dirs(path = projDir, full.names = FALSE)
 samples <- samples[-1] # may not need if we call this within the project directory
+N <- length(samples)
 
 message(str_glue("Found all samples: "))
 message(str_glue("{samples}", .sep = " "))
@@ -19,11 +20,11 @@ message(str_glue("{samples}", .sep = " "))
 # generalize everything below to be performed per each sample
 
 # These lists include the relevant data for each sample in the project
-ABR_thresholds <- vector("list", length(samples))
-ABR_peaks <- vector("list", length(samples))
-ABR_waveforms <- vector("list", length(samples))
-DP_thresholds <- vector("list", length(samples))
-DP_peaks <- vector("list", length(samples))
+ABR_thresholds <- vector("list", N)
+ABR_peaks <- vector("list", N)
+ABR_waveforms <- vector("list", N)
+DP_thresholds <- vector("list", N)
+DP_peaks <- vector("list", N)
 
 names(ABR_thresholds) <- samples
 names(ABR_peaks) <- samples
@@ -31,10 +32,9 @@ names(ABR_waveforms) <- samples
 names(DP_thresholds) <- samples
 names(DP_peaks) <- samples
 
-for (i in 1:length(samples)) {
+for (i in 1:N) {
   cur_sample <- str_glue("{projDir}/{samples[i]}")
-
-  valid_sample(projDir, cur_sample)
+  message(str_glue("Currently working on {cur_sample}"))
 
   ABR_analyzed_fp <- list.files(
     cur_sample,
@@ -57,6 +57,27 @@ for (i in 1:length(samples)) {
   )
   DP_wave_fp <- DP_wave_fp[1]
 
+  expected_size <- readLines(str_glue(
+    "{cur_sample}/Experiment State.txt"
+  ))
+
+  num_ABRs <- as.numeric(str_extract(expected_size[3], "[0-9]+"))
+  num_DPs <- as.numeric(str_extract(expected_size[4], "[0-9]+"))
+
+  if (
+    num_ABRs != length(ABR_analyzed_fp) || num_DPs != length(DP_analyzed_fp)
+  ) {
+    stop(
+      "The wrangler did not find the expected number of ABR or DP files for sample {cur_sample}"
+    )
+  }
+
+  if (num_DPs > 1) {
+    stop(
+      "We can not handle more than one DP reading at this time, sample {cur_sample} has {num_DPs} readings"
+    )
+  }
+
   for (j in 1:num_ABRs) {
     # extract ABR data, adding the threshold to the sample threshold data
     cur_file <- str_glue("{cur_sample}/{ABR_analyzed_fp[j]}")
@@ -71,7 +92,9 @@ for (i in 1:length(samples)) {
     # Read the data as a tsv skipping the header, add frequecny for the data
     ABR_analyzed <- read.delim(cur_file, sep = "\t", skip = 6)
     ABR_analyzed <- ABR_analyzed |>
-      mutate(frequency, rep(freq, n()))
+      mutate(frequency = rep(freq, n()))
+
+    # filter the trailing empty data column
 
     rm(cur_file)
 
@@ -80,60 +103,77 @@ for (i in 1:length(samples)) {
 
     levels <- unlist(str_extract_all(header[6], "\\d+"))
 
-    ABR_wave <- read_delim(cur_file, delim = "\t", skip = 7, col_names = FALSE)
-    colnames(data) <- levels
+    ABR_wave <- read.delim(cur_file, sep = "\t", skip = 7, header = FALSE)
+    colnames(ABR_wave) <- levels
     ABR_wave <- ABR_wave |>
-      mutate(frequency, rep(freq, n()))
+      mutate(frequency = rep(freq, n()))
 
     # if j=1 add dataframe to the ABR_peaks list, else colbind
     # writing the data to organized format
     if (j == 1) {
-      ABR_thresholds$cur_sample <- threshold
-      ABR_peaks$cur_sample <- ABR_analyzed
-      ABR_waveforms$cur_sample <- ABR_wave
+      ABR_thresholds[[samples[i]]] <- threshold
+      ABR_peaks[[samples[i]]] <- ABR_analyzed
+      ABR_waveforms[[samples[i]]] <- ABR_wave
     } else {
-      ABR_thresholds$cur_sample <- rbind(ABR_thresholds$cur_sample, threshold)
-      ABR_peaks$cur_sample <- rbind(ABR_peaks$cur_sample, ABR_analyzed)
-      ABR_waveforms$cur_sample <- rbind(ABR_waveforms$cur_sample, ABR_wave)
+      ABR_thresholds[[samples[i]]] <- rbind(
+        ABR_thresholds[[samples[i]]],
+        threshold
+      )
+      ABR_peaks[[samples[i]]] <- bind_rows(
+        ABR_peaks[[samples[i]]],
+        ABR_analyzed
+      )
+      ABR_waveforms[[samples[i]]] <- bind_rows(
+        ABR_waveforms[[samples[i]]],
+        ABR_wave
+      )
     }
   }
 
   # The DP files come pre-stacked, so I just need to extract once per sample and add it to the list
-  cur_file <- str_glue("{cur_sample}/{DP_analyzed_fp[i]}")
+  message(str_glue("On to the DP data"))
+  message(str_glue("The current file is {cur_sample}/{DP_analyzed_fp}"))
+  cur_file <- str_glue("{cur_sample}/{DP_analyzed_fp}")
 
   DP_thresh <- read_delim(cur_file, delim = "\t")
 
-  cur_file <- str_glue("{cur_sample}/{DP_wave_fp[i]}")
+  message(str_glue("The current file is {cur_sample}/{DP_wave_fp}"))
+  cur_file <- str_glue("{cur_sample}/{DP_wave_fp}")
   # Header row 6 is the colnames, but rows 1-5 and row 7 are not needed so, we need
   # to read all the lines as data first then subset then read the data as a delim
   DP_wave <- readLines(cur_file)
-  DP_wave <- data[-c(1:5, 7)]
-  DP_wave <- read_delim(I(data), delim = "\t")
+  DP_wave <- DP_wave[-c(1:5, 7)]
+  DP_wave <- read_delim(I(DP_wave), delim = "\t")
 
   # save the output to it's sample name
-  DP_thresholds <- DP_thresh
-  DP_peaks <- DP_wave
+  DP_thresholds[[samples[i]]] <- DP_thresh
+  DP_peaks[[samples[i]]] <- DP_wave
+
+  rm(list = c('ABR_analyzed', 'ABR_wave', 'DP_thresh', 'DP_wave'))
 }
 
-# this can then be stored to the sample name in the list for further post-processing
-
-# the process will include getting user input and then putting it all together in
-# excel sheet like Luis wants for prism usage
-
 # User input ----
-
+# TODO:
 # Prompt the user for how many peaks deep they would like the wrangled data to include
 # this refers to which peaks to determine rawwaveforms for
 # Prompt the user for the dB levels they would like amplitudes and latencies to
 # be wrangled for
 
-# ABR thresholds ----
-# Make ABR threshold table by reading the top few lines of each ABR-analyzed.txt
-# file, cut the headers from each file and turn the remaining matrix into a table
-# named with the frequency
+# Merge and format ----
+# Merge all samples in list ABR_thresholds, merge by col 1
+ABR_thresh_exp <- ABR_thresh_table(ABR_thresholds)
 
-# DP thresholds
-# make the DP thresholds by merging the DP-samplename-1-analyzed.txt file for each
-# sample
+DP_thresh_exp <- DP_thresh_table(DP_thresholds)
 
-#Peak Organization
+# write to excel
+
+# wrap this in a loop for the level the user wants peaks and latencies for,
+# just directly write them in order as sheets to the excel
+ABR_amplitudes <- ABR_extract_amplitude(80, 1, ABR_peaks)
+ABR_latencies <- ABR_extract_latency(80, 1, ABR_peaks)
+
+# now Luis also wants the amplitude extraction along all levels for each of the
+# of the frequencies as it's own sheet, could make the extraction functions
+# polymorphic: e.g. if given a level and a peak, extract the information across
+# all frequencies, but if given a frequency and a peak, extract it across all levels
+# actually seems quite easy just a bit of logic to determine the function to filter by...
--- utils.R
+++ utils.R
@@ -1,27 +1,90 @@
-# Utilities for the ABR wrangler that could be useful to resuse
+# Utilities for the ABR wrangler that could be useful to reuse
+require(tidyverse)
 
-# Validates the available files for a single sample, testing the number of available
-# ABR and DP files against the experiment state file
-valid_sample <- function(projDir, cur_sample) {
-  expected_size <- readLines(str_glue(
-    "{projDir}/{cur_sample}/Experiment State.txt"
-  ))
+# Make table out of list of ABR threshold tables
+# takes in a list of threshold data for a number of ABR samples and concatenates into a table
+ABR_thresh_table <- function(tl) {
+  n <- length(tl)
 
-  num_ABRs <- as.numeric(str_extract(expected_size[3], "[0-9]+"))
-  num_DPs <- as.numeric(str_extract(expected_size[4], "[0-9]+"))
-
-  # can use these numbers as the iterators for the internal loop
-  if (
-    num_ABRs != length(ABR_analyzed_fp) || num_DPs != length(DP_analyzed_fp)
-  ) {
-    stop(
-      "The wrangler did not find the expected number of ABR or DP files for sample {cur_sample}"
-    )
+  table <- data.frame(tl[[1]])
+  for (i in 2:n) {
+    table <- table |>
+      full_join(data.frame(tl[[i]]), by = "X1")
   }
 
-  if (num_DPs > 1) {
-    stop(
-      "We can not handle more than one DP reading at this time, sample {cur_sample} has {num_DPs} readings"
-    )
-  }
+  colnames(table) <- c("Frequency (kHz)", names(tl))
+  return(table)
 }
+
+DP_thresh_table <- function(tl) {
+  n <- length(tl)
+
+  table <- data.frame(tl[[1]])
+  for (i in 2:n) {
+    table <- table |>
+      full_join(data.frame(tl[[i]]), by = "f2")
+  }
+
+  colnames(table) <- c("f2", names(tl))
+  return(table)
+}
+
+# Given a level and a peak and a list of analyzed ABR data, extract relevant data
+# and return the dataframe
+ABR_extract_amplitude <- function(level, peak, pl) {
+  n <- length(pl)
+
+  # TODO:
+  # some logic to determine the peak chosen, ensure it's an integer, then just glue it together, while you're at it check that the level is numeric
+  to_select <- str_glue("P{peak}.Amplitude")
+
+  table <- data.frame(pl[[1]]) |>
+    select(Level, frequency, {{ to_select }}) |>
+    filter(Level == level) |>
+    select(!Level)
+
+  for (i in 2:n) {
+    new <- data.frame(pl[[i]]) |>
+      select(Level, frequency, {{ to_select }}) |>
+      filter(Level == level) |>
+      select(!Level)
+
+    table <- table |>
+      full_join(new, by = "frequency")
+  }
+
+  colnames(table) <- c("Frequency", names(pl))
+  return(table)
+}
+
+# will be just about the same as ABR_extract_peak, so could maybe make the latency
+# or peak data extraction be a parameter
+ABR_extract_latency <- function(level, peak, pl) {
+  n <- length(pl)
+
+  # steal the logic from above or combine the functions
+  to_select <- str_glue("P{peak}.Latency")
+
+  table <- data.frame(pl[[1]]) |>
+    select(Level, frequency, {{ to_select }}) |>
+    filter(Level == level) |>
+    select(!Level)
+
+  for (i in 2:n) {
+    new <- data.frame(pl[[i]]) |>
+      select(Level, frequency, {{ to_select }}) |>
+      filter(Level == level) |>
+      select(!Level)
+
+    table <- table |>
+      full_join(new, by = "frequency")
+  }
+
+  colnames(table) <- c("Frequency", names(pl))
+  return(table)
+}
+
+
+ABR_wave <- function() {}
+
+DP_wave <- function() {}
